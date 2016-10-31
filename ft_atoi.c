ff229a5139013d720ed1e76cc484f55fcc567ce4
Ming Horn <mhorn@e1z2r3p26.42.us.org>
2016-10-31 14:20:47 -0700

    Pushing everything so I can work remotely

---
+++ ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhorn <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/15 09:30:25 by mhorn             #+#    #+#             */
+/*   Updated: 2016/08/23 18:31:28 by mhorn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		is_ok(char c, int str_num, int *ok_ptr)
+{
+	int count;
+
+	count = 0;
+	if (str_num > 0)
+		return (1);
+	if (c == '\n' || c == '\t' || c == '\v' || c == '\r' || c == '\f')
+		return (0);
+	if ((c == '-' || c == '+') && *ok_ptr == 0)
+	{
+		*ok_ptr += 1;
+		return (0);
+	}
+	if (c == ' ')
+		return (0);
+	return (1);
+}
+
+int		ft_atoi(char *str)
+{
+	int str_num;
+	int index;
+	int is_neg;
+	int ok;
+	int *ok_ptr;
+
+	str_num = 0;
+	ok = 0;
+	ok_ptr = &ok;
+	index = 0;
+	is_neg = 1;
+	while (str[index] != '\0')
+	{
+		if (!(str[index] < '0') && !(str[index] > '9'))
+			str_num = str_num * 10 + (str[index] - '0');
+		else if (is_ok(str[index], str_num, ok_ptr) != 0)
+			return (str_num * is_neg);
+		else if (str[index] == '-')
+			is_neg = -1;
+		index++;
+	}
+	return (str_num * is_neg);
+}
---
+++ ft_tolower.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhorn <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/16 20:14:29 by mhorn             #+#    #+#             */
+/*   Updated: 2016/08/16 20:14:33 by mhorn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*create_array(int size, char *str)
+{
+	char	arr[size + 1];
+	int		in;
+
+	in = 0;
+	while (str[in] != '\0')
+	{
+		if (str[in] >= 65 && str[in] <= 90)
+			arr[in] = str[in] + 32;
+		else
+			arr[in] = str[in];
+		in++;
+	}
+	arr[in] = '\0';
+	str = arr;
+	return (str);
+}
+
+char	*ft_strlowcase(char *str)
+{
+	int in;
+
+	in = 0;
+	while (str[in] != '\0')
+	{
+		in++;
+	}
+	return (create_array(in, str));
+}
