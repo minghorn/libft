0703f6d3f803d5529dfb58b3c72a51bf0b15b971
Ming Horn <minghaihorn@gmail.com>
2016-11-07 13:45:45 -0800

    Started memmove

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhorn <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/07 13:27:16 by mhorn             #+#    #+#             */
+/*   Updated: 2016/11/07 13:32:19 by mhorn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*dstm;
+	unsigned char	*srcm;
+
+	dstm = (unsigned char*)dst;
+	srcm = (unsigned char*)src;
+
+	return (dst);
+}
