d074b639144863063bd78fcf854a2eb54352d5e8
Ming Horn <minghaihorn@gmail.com>
2016-11-22 12:53:22 -0800

    Pushing all new work on strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,4 +1,24 @@
 #include "libft.h"
+#include <stdio.h>
+
+static int	word_count(char const *s, char c)
+{
+	int	i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i + 1] == c || s[i + 1] == '\0') && s[i] != c)
+		{
+//			printf("s[i]: %c s[i + 1]: %c\n", s[i], s[i+1]);
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
 
 static char	*next_word(int *i, char const *s, char c)
 {
@@ -19,6 +39,7 @@ static char	*next_word(int *i, char const *s, char c)
 		while (s[*i] == c && s[*i] != '\0')
 			*i = *i + 1;
 	}
+//	printf("%s ", word);
 	return (word);
 }
 
@@ -31,11 +52,13 @@ char	**ft_strsplit(char const *s, char c)
 	i = malloc(sizeof(int) * 1);
 	*i = 0;
 	j = 0;
-	arr = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1));
-	while (s[*i] != '\0')
+	arr = (char **)malloc(sizeof(char *) * (word_count(s, c)));
+//	printf("%d\n", word_count(s, c));
+	while (s[*i] != '\0' && arr != NULL && i != NULL)
 	{
 		arr[j] = next_word(i, s, c);
 		j++;
 	}
+//	printf("\n");
 	return (arr);
 }
