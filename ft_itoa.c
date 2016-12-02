6362f05101262edf67309d7f87ec1cd0b400134f
Ming Horn <minghaihorn@gmail.com>
2016-12-02 12:20:37 -0800

    Pushign updates

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,41 +12,6 @@
 
 #include "libft.h"
 
-static char		char_int(int n)
-{
-	char	i;
-
-	i = '0';
-	while (i != n + '0')
-		i++;
-	return (i);
-}
-
-static int		num_digits(int n)
-{
-	int		neg;
-	int		digits;
-
-	neg = 0;
-	digits = 0;
-	if (n < 0)
-	{
-		neg = 1;
-		if (n != -2147483648)
-			n = n * -1;
-		else
-			return (11);
-	}
-	else if (n == 0)
-		return (1);
-	while (n > 0)
-	{
-		digits++;
-		n = n / 10;
-	}
-	return (digits + neg);
-}
-
 char			*ft_itoa(int n)
 {
 	char	*num;
@@ -54,7 +19,7 @@ char			*ft_itoa(int n)
 	int		i;
 	int		neg;
 
-	len = num_digits(n);
+	len = ft_ctdigits(n);
 	neg = -1;
 	num = (char *)malloc(sizeof(char) * (len + 1));
 	if (num != NULL)
@@ -78,7 +43,7 @@ char			*ft_itoa(int n)
 		i = len - 1;
 		while (i > neg)
 		{
-			num[i] = char_int(n % 10);
+			num[i] = ft_itoc(n % 10);
 			n = n / 10;
 			i--;
 		}
