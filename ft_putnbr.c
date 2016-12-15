57e9547ab23f5dcfd0b4f4e86b860bef04ef8965
Ming Horn <minghaihorn@gmail.com>
2016-12-14 19:44:41 -0800

    Check bugs at 42, determine ft_putnbr test

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -12,46 +12,7 @@
 
 #include "libft.h"
 
-static void	print_num(int n[12], int len)
-{
-	int		i;
-
-	i = 0;
-	while (i < len)
-	{
-		ft_putchar(n[i] + '0');
-		i++;
-	}
-    ft_putchar('\n');
-}
-
 void		ft_putnbr(int n)
 {
-    int		neg;
-    int 	num[12];
-    int		len;
-    int		i;
-    
-	neg = 0;
-	len = ft_ctdigits(n);
-    if (n == -2147483648)
-		write(1, "-2147483648", 12);
-    else if (n < 0)
-    {
-        neg = 1;
-        n = n * -1;
-    }
-	i = len - 1;
-	num[len] = '\0';
-    while (i > 0)
-    {
-        num[i] = n % 10;
-        n = n / 10;
-		i--;
-    }
-	if (neg)
-		num[0] = '-';
-	else
-		num[0] = n;
-    print_num(num, len);
+	ft_putnbr_fd(n, 1);
 }
--- lft_test.c
+++ lft_test.c
@@ -201,7 +201,27 @@ int  main(void)
 	mt_test_strstr("", "");
 	mt_test_strstr(buf, "une");
 	mt_test_strstr("J'ai fait pipapipapou en LV2", "pipapou"); */
+	int		out;
+	int		p[2];
+	char	buf[100];
 
-	ft_putnbr(01-11230010203-562147483647-2147483648);
+	out = dup(1);
+	pipe(p);
+	dup2(p[1], 1);
+	ft_putnbr(0);
+	ft_putnbr(1);
+	ft_putnbr(-1);
+	ft_putnbr(12300);
+	ft_putnbr(10203);
+	ft_putnbr(-56);
+	ft_putnbr(2147483647);
+	ft_putnbr(-2147483648);
+	dup2(out, 1);
+	buf[read(p[0], buf, 100)] = 0;
+	close(p[0]);
+	close(p[1]);
+	close(out);
+//	ft_putnbr(01-11230010203-562147483647-2147483648);
+	printf("%d\n", strcmp(buf, "01-11230010203-562147483647-2147483648"));
 	return (0);
 }
