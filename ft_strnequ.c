fcee7ac998343be361f13b0844cce7b600d58c3c
Ming Horn <minghaihorn@gmail.com>
2016-12-26 23:13:50 -0800

    Fixed ft_strnequ for ytest

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,6 +17,8 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t i;
 
 	i = 0;
+	if (n == 0)
+		return (1);
 	if (!s1 || !s2)
 		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
@@ -25,5 +27,9 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 			return (0);
 		i++;
 	}
+	if (i < n && s1[i] != s2[i])
+		return (0);
+	if ((s1[i] == '\0' && s2[i] != '\0') || (s2[i] == '\0' && s1[i] != '\0'))
+		return (0);
 	return (1);
 }
