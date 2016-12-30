8d05a103307e6ac67df226ec4628b3db64db79f2
Ming Horn <minghaihorn@gmail.com>
2016-12-29 22:18:34 -0800

    Literally dying, why doesn't it pass maintest

--- ft_memmove.c
+++ ft_memmove.c
@@ -16,42 +16,32 @@
 ** Check in which direction to copy in, if srcm < dstm copy right to left
 ** if srcm > dstm copy left to right otherwise they are equal and do not copy
 */
-
-static int	overlapping(unsigned char *dstm, unsigned char *srcm)
+static void	*cpy_s(void *dst, const void *src, size_t len)
 {
-	if (srcm < dstm)
-		return (-1);
-	else if (srcm > dstm)
-		return (1);
-	return (0);
-}
-
-static void	cpy_s(unsigned char *dstm, unsigned char *srcm, size_t len, int d)
-{
-	int		i;
-
-	if (d > 0)
-		ft_memcpy(dstm, srcm, len);
-	else if (d < 0)
-	{
-		i = (int)len - 1;
-		dstm[(int)len] = '\0';
-		while (i > -1)
-		{
-			dstm[i] = srcm[i];
-			i--;
-		}
-	}
-}
-
-void		*ft_memmove(void *dst, const void *src, size_t len)
-{
+	int				i;
 	unsigned char	*dstm;
 	unsigned char	*srcm;
 
 	dstm = (unsigned char *)dst;
 	srcm = (unsigned char *)src;
+	i = (int)len - 1;
+	dstm[(int)len] = '\0';
+	while (i > -1)
+	{
+		dstm[i] = srcm[i];
+		i--;
+	}
+	return (dst);
+}
+
+void		*ft_memmove(void *dst, const void *src, size_t len)
+{
 	if (len)
-		cpy_s(dstm, srcm, len, overlapping(dstm, srcm));
+	{
+		if (dst > src && dst <= src + len)
+			return (cpy_s(dst, src, len));
+		else
+			return (ft_memcpy(dst, src, len));
+	}
 	return (dst);
 }
